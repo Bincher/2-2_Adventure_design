c108507b71383e70506ba64a6bf8623e337a1911
Bincher <98527148+Bincher@users.noreply.github.com>
2022-11-02 16:46:20 +0900

    중급과제 - 메모리관리자(설명추가)

--- adventure_design/main.cpp
+++ adventure_design/main.cpp
@@ -5,7 +5,6 @@
 #include <vector>
 using namespace std;
 
-
 // 참고 : https://velog.io/@stellakim1012/%EA%B0%84%EB%8B%A8%ED%95%9C-%EB%A9%94%EB%AA%A8%EB%A6%AC-%EA%B4%80%EB%A6%AC%EC%9E%90
 
 typedef struct chunk {
@@ -77,58 +76,66 @@ int myalloc_best(Chunk* available, int request_size)
 {
 	total += 1;
 
-	Chunk* before = head;
-	Chunk* current = head;
+	Chunk* before = head; //초기화
+	Chunk* current = head; //초기화
 	int temp = 0;
 	int count = 0;
-	int index = 0;
+	int index = 0; //return값
 	int diff = 100;
-
+	
+	// 1. 찾는 과정
 	while (current != NULL)
 	{
+		// 두가지를 동시에 만족
+		// current가 지목한 chunk의 사이즈가 
+		// 1. 요청받은 사이즈와의 차이가 작은지
+		// 2. 요청받은 사이즈보다 큰지
 		if (((current->size - request_size) < diff) && (current->size >= request_size))
 		{
-			temp = current->start;
-			diff = current->size - request_size;
-			index = count;
-			current = current->link;
+			temp = current->start; //temp는 current가 지목한 chunk로
+			diff = current->size - request_size; //diff는 이제 또 다른 기준점으로
+			index = count; //chunk가 몇번째 노드인지 확인
+			current = current->link; //current는 다음 노드를 지목
 		}
 		else
 		{
-			if (current->link == NULL && before->size < request_size)
+			// 만약 끝까지 갔음에도 적절한 노드가 없다면
+			if (current->link == NULL && before->size < request_size) 
 			{
 				fail += 1;
 				return -1;
 				break;
 			}
-			current = current->link;
+			current = current->link; // 끝까지 간거 아니면 current는 다음 노드로 이동
 		}
 
-		count += 1;
+		count += 1; // index 확인을 위해
 	}
 
-	before = head;
-	current = head;
-
-	for (int i = 0; i < index; i++)
+	
+	before = head; // 초기화
+	current = head; // 초기화
+	
+	// 2. 할당 과정
+	for (int i = 0; i < index; i++) // 해당되는 노드까지 이동
 	{
-		before = current;
-		current = current->link;
+		before = current; // 해당되는 노드
+		current = current->link; // 해당되는 노드의 다음 노드
 	}
 
-	if (diff == 0)
+	if (diff == 0) // 해당 공간이 원하는 사이즈랑 같으면
 	{
-		before->link = current->link;
-		current = current->link;
-		if (head->size == request_size)
+		before->link = current->link; // 해당 노드의 이전노드와 다음노드를 연결
+		current = current->link; 
+		if (head->size == request_size) // 만약 그게 맨 첫 노드이면
 		{
-			head = head->link;
+			head = head->link; // 기존 노드는 불필요하므로 다음 노드를 head가 지목
 		}
 	}
-	else
+	else // 공간이 남을시
 	{
-		temp = current->start;
-		current->start += request_size;
+		temp = current->start; // temp에 start위치 저장(return 용도)
+		current->start += request_size; // 그 과정
 		current->size -= request_size;
 	}
 
@@ -145,58 +152,67 @@ void myfree(Chunk* available, int start_address, int return_size) // 그림 필수일
 	Chunk* next = temp->link; // 두 next변수엔 첫 청크의 다음 것
 	Chunk* next2 = next;
 
-	int case_num = 0;
-	int case_num2 = 0;
+	int case_num = 0; // 반환여부
+	int case_num2 = 0; // 합병여부
 
-	if (next == NULL) // 청크의 다음 것이 없다면?
+	if (next == NULL) // 노드가 하나
 	{
 		case_num = 1;
 		case_num2 = 1;
 	}
 
+
+	// 반환 과정
 	switch (case_num)
 	{
-	case 0: // chunk의 next가 있다
+	case 0: // 노드가 2개 이상
 		while (1)
 		{
-			if (next == NULL || (next->start > newchunk->start)) // next의 시작지점 > 삭제할 데이터의 시작지점
+			// next의 시작지점 > 삭제할 데이터의 시작지점
+			// 오름차순 맞추기
+			if (next == NULL || (next->start > newchunk->start)) 
 			{
-				newchunk->link = temp->link; // newchunk는 temp(원래의 청크) 다음에 위치
-				temp->link = newchunk; // temp(원래의 청크)를 newchunk와 연결
+				// temp와 next사이에 들어가도록
+				newchunk->link = temp->link; // newchunk 뒤로 temp의 다음 노드와 연결 
+				temp->link = newchunk; // temp의 다음 노드는 newchunk로 
 				break;
 			}
 			else  // next의 시작지점 < 삭제할 데이터의 시작지점
 			{
-				temp = next; // 원래의 청크는 next가 가리키는걸로 바꾸고
-				next = next->link; // next는 next의 다음것을 지목
+				temp = next; // temp는 다음노드로
+				next = next->link; // next도 다음노드로
 			}
 		}
 		break;
-	case 1:
-		temp->link = newchunk;
+	case 1: // 노드가 한개
+		temp->link = newchunk; // 새 노드를 연결
 		newchunk->link = NULL;
 		break;
 	}
 
+	// 합병 과정
 	switch (case_num2)
 	{
-	case 0:
+	case 0: // 노드가 두개 이상
 		while (next2 != NULL)
 		{
+			// 합병해야할 노드인지 확인
 			if (temp2->start + temp2->size == next2->start)
 			{
-				temp2->link = next2->link;
-				temp2->size += next2->size;
-				next2 = next2->link;
+				// temp2는 대상노드의 이전노드
+				// next2는 대상노드
+				temp2->link = next2->link; // 합병 대상 노드의 앞과 뒤를 연결
+				temp2->size += next2->size; // 이전노드의 size 증가
+				next2 = next2->link; // next2는 다음 노드로 이동
 			}
 			else
 			{
-				temp2 = next2;
+				temp2 = next2; // 한칸씩 이동
 				next2 = next2->link;
 			}
 		}
 		break;
-	case 1:
+	case 1: // 노드가 한개 -> 할 필요 없음
 		break;
 	}
 }
@@ -234,9 +250,11 @@ void main()
 	int index = 0;
 	Chunk available; // 할당및 저장을 할 연결리스트 available
 	Init(&available);
+
 	vector <int> start_v;
 	vector <int> size_v;
 	vector <char> index_v;
+
 	for (int i = 0; i < 10; i++)
 	{
 		int size_temp = 0;
