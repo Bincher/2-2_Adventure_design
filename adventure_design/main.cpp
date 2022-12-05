2cf4da2895072e12fa8ae79e57ac751c47d1b709
김성빈 <20190207@office.kumoh.ac.kr>
2022-12-05 15:05:13 +0900

    기말플젝 미완

--- adventure_design/main.cpp
+++ adventure_design/main.cpp
@@ -1,309 +1,326 @@
+#pragma warning(disable:4996)
 #include <iostream>
-#include <time.h>
-#include <random>
-#include <math.h>
+#include <fstream>
+#include <unordered_map>
 #include <vector>
+#include <algorithm>
+#include <queue>
+#include <bitset>
+
+#define BITSNUMBYTE 8
+
 using namespace std;
 
-// 참고 : https://velog.io/@stellakim1012/%EA%B0%84%EB%8B%A8%ED%95%9C-%EB%A9%94%EB%AA%A8%EB%A6%AC-%EA%B4%80%EB%A6%AC%EC%9E%90
-
-typedef struct chunk {
-	int start; // 메모리 덩어리의 시작 주소
-	int size;  // 메모리 덩어리의 크기
-	struct chunk* link; // 다음 빈 공간에 대한 링크
-}Chunk;
-
-Chunk* head; // 연결리스트의 헤더 포인터
-
-int total = 0;
-int fail = 0;
-
-void Init(Chunk* available)
+// 허프만 트리를 구성할 노드
+class Node 
 {
-	available->start = 0;
-	available->size = 100;
-	available->link = NULL;
-	head = available; // head의 start, size, link를 초기화하기 위한 초석
+public:
+    char mCharacter; // 글자
+    int mFreq; // 빈도수(합)
+    Node* left = NULL; // 왼쪽가지
+    Node* right = NULL; // 오른쪽가지
 
-	printf("start = %d \n", available->start);
-	printf("size = %d \n\n", available->size);
+    // 노드 생성
+    Node(char character, int value, Node* nLeft = NULL, Node* nRight = NULL) {
+        mCharacter = character;
+        mFreq = value;
+        left = nLeft;
+        right = nRight;
+    }
+};
+
+
+// 우선순위 큐를 사용하기 위한 비교함수
+struct cmp 
+{
+    // 내림차순 만들기
+    bool operator()(Node& a, Node& b) {
+        return a.mFreq > b.mFreq;
+    }
+};
+
+
+// ********************
+// @ makeTree
+// @ 우선순위 큐(priority_queue)를 이용해 tree 만듦
+// @ 1. 최소 빈도수 노드 두 개 고르고, 우선순위 큐에서 pop
+// @ 2. 최소 빈도수 노드 두 개의 합으로 노드를 하나 만들고, 두 개의 노드는 자식으로 만듦
+// @ 3. 만든 노드를 우선순위 큐에 push
+// ********************
+/*
+* 우선순위큐
+* priority_queue<자료형, 구조체, 비교함수>
+*/
+void makeTree(priority_queue<Node, vector<Node>, cmp>& p) 
+{
+    while (true) {
+        // size_t == unsigned int == 부호없는 32(or 64)bit 정수, 메모리/문자열 길이 구할때 사용
+        size_t size = p.size(); // 노드의 개수
+        if (size < 2) break; // 노드의 개수가 2개 이상이여야 가능
+
+        // p.top이 가장 작은 mValue값을 가짐을 이용 -> 노드를 생성후 pop
+        Node* firstMinNode = new Node(p.top().mCharacter, p.top().mFreq, p.top().left, p.top().right);
+        //printf("%c |", p.top().mCharacter);
+        p.pop();
+        Node* secondMinNode = new Node(p.top().mCharacter, p.top().mFreq, p.top().left, p.top().right);
+        //printf("%c ||", p.top().mCharacter);
+        p.pop();
+        int newValue = firstMinNode->mFreq + secondMinNode->mFreq; // 새로 생긴 부모노드의 mValue값 만들기
+        
+        Node* newNode = new Node(NULL, newValue); // 두 노드의 부모노드를 생성
+        newNode->left = firstMinNode; // 자식노드와 연결
+        newNode->right = secondMinNode;
+        p.push(*newNode); // 우선순위 큐에 넣어주기
+    }
 }
 
-int myalloc(Chunk* available, int request_size) // first-fit
+
+// ********************
+// @ characterMapping
+// @ mapping char-string. using unordered_map<char, string> characterMap
+// @ tree(node)를 받아서 tree를 순회하면서 mapping 수행
+// ********************
+void characterMapping(string str, Node node, unordered_map<char, string>& characterMap) 
 {
-	total += 1; // 메모리 할당 횟수
-	int temp = 0; // 시작 위치 저장
-	Chunk* before = head; // before 초기화
-	Chunk* current = head; // current 초기화
+    // 맨 처음 str은 공백
+    if (node.mCharacter != NULL) // mCharacter가 있다 == 마지막 노드이다(부모노드를 만들때 mcharacter은 Null)
+    {
+        characterMap[node.mCharacter] = str; // 이동하면서 만들어진 코드를 저장
+        //cout << node.mCharacter << endl;
+        //cout << str << endl;
+        return; // 재귀함수 종료
+    }
 
-	while (1)
-	{
-		if (current->size >= request_size) // 현재 사이즈가 요청받은 사이즈보다 크거나 같다 == 할당이 가능하다
-		{
-			temp = current->start; // temp에 start지점을 임시로 저장
-			current->start += request_size; // start지점에 요청한 사이즈만큼 더해주기 -> 다음 할당은 더해진 만큼의 start위치에서 시작
-			if (current->size == request_size) // 만약 남은 사이즈가 요청받은 사이즈랑 같으면?
-			{
-				current->size -= request_size; // size는 요청받은 사이즈만큼 감소
-				before = current->link; // before는 다음 chunk를 가르킴
-				current = current->link; // current는 이제 그다음 chunk
-			}
-			else
-			{
-				current->size -= request_size; // size는 요청받은 사이즈만큼 감소
-			}
-			if (head->start == 100) // 만약 첫 청크의 start가 100이면?
-			{
-				head = head->link; // head를 다음 청크와 연결
-			}
-			break;
-		}
-		else // 들어갈 용량이 없음!
-		{
-			before = current; // before는 current를 가르킴
-			current = current->link; // current를 다음 chunk로 이동
-			if (current == NULL) // 만약 다음 chunk가 없으면->실패
-			{
-				fail += 1;
-				return -1;
-				break;
-			}
-		}
-	}
-	return temp;
-}
-int myalloc_best(Chunk* available, int request_size)
-{
-	total += 1;
-
-	Chunk* before = head; //초기화
-	Chunk* current = head; //초기화
-	int temp = 0;
-	int count = 0;
-	int index = 0; //return값
-	int diff = 100;
-	
-	// 1. 찾는 과정
-	while (current != NULL)
-	{
-		// 두가지를 동시에 만족
-		// current가 지목한 chunk의 사이즈가 
-		// 1. 요청받은 사이즈와의 차이가 작은지
-		// 2. 요청받은 사이즈보다 큰지
-		if (((current->size - request_size) < diff) && (current->size >= request_size))
-		{
-			temp = current->start; //temp는 current가 지목한 chunk로
-			diff = current->size - request_size; //diff는 이제 또 다른 기준점으로
-			index = count; //chunk가 몇번째 노드인지 확인
-			current = current->link; //current는 다음 노드를 지목
-		}
-		else
-		{
-			// 만약 끝까지 갔음에도 적절한 노드가 없다면
-			if (current->link == NULL && before->size < request_size) 
-			{
-				fail += 1;
-				return -1;
-				break;
-			}
-			current = current->link; // 끝까지 간거 아니면 current는 다음 노드로 이동
-		}
-
-		count += 1; // index 확인을 위해
-	}
-
-	
-	before = head; // 초기화
-	current = head; // 초기화
-	
-	// 2. 할당 과정
-	for (int i = 0; i < index; i++) // 해당되는 노드까지 이동
-	{
-		before = current; // 해당되는 노드
-		current = current->link; // 해당되는 노드의 다음 노드
-	}
-
-	if (diff == 0) // 해당 공간이 원하는 사이즈랑 같으면
-	{
-		before->link = current->link; // 해당 노드의 이전노드와 다음노드를 연결
-		current = current->link; 
-		if (head->size == request_size) // 만약 그게 맨 첫 노드이면
-		{
-			head = head->link; // 기존 노드는 불필요하므로 다음 노드를 head가 지목
-		}
-	}
-	else // 공간이 남을시
-	{
-		temp = current->start; // temp에 start위치 저장(return 용도)
-		current->start += request_size; // 그 과정
-		current->size -= request_size;
-	}
-
-	return temp;
+    str += "0"; // 왼쪽 노드로 갈땐 0을
+    characterMapping(str, *node.left, characterMap);
+    str[str.size() - 1] = '1'; // 오른쪽 노드로 갈땐 1을
+    characterMapping(str, *node.right, characterMap);
 }
 
-void myfree(Chunk* available, int start_address, int return_size) // 그림 필수일듯
-{
-	Chunk* newchunk = (Chunk*)malloc(sizeof(Chunk)); // 삭제를 함으로서 생기는 chunk를 표현
-	newchunk->start = start_address; // chunk의 start는 기존 데이터의 start랑 같다
-	newchunk->size = return_size; // chunk의 size도 마찬가지
-	Chunk* temp = available; // 두 임시변수엔 첫 번째 청크
-	Chunk* temp2 = temp;
-	Chunk* next = temp->link; // 두 next변수엔 첫 청크의 다음 것
-	Chunk* next2 = next;
 
-	int case_num = 0; // 반환여부
-	int case_num2 = 0; // 합병여부
-
-	if (next == NULL) // 노드가 하나
-	{
-		case_num = 1;
-		case_num2 = 1;
-	}
-
-
-	// 반환 과정
-	switch (case_num)
-	{
-	case 0: // 노드가 2개 이상
-		while (1)
-		{
-			// next의 시작지점 > 삭제할 데이터의 시작지점
-			// 오름차순 맞추기
-			if (next == NULL || (next->start > newchunk->start)) 
-			{
-				// temp와 next사이에 들어가도록
-				newchunk->link = temp->link; // newchunk 뒤로 temp의 다음 노드와 연결 
-				temp->link = newchunk; // temp의 다음 노드는 newchunk로 
-				break;
-			}
-			else  // next의 시작지점 < 삭제할 데이터의 시작지점
-			{
-				temp = next; // temp는 다음노드로
-				next = next->link; // next도 다음노드로
-			}
-		}
-		break;
-	case 1: // 노드가 한개
-		temp->link = newchunk; // 새 노드를 연결
-		newchunk->link = NULL;
-		break;
-	}
-
-	// 합병 과정
-	switch (case_num2)
-	{
-	case 0: // 노드가 두개 이상
-		while (next2 != NULL)
-		{
-			// 합병해야할 노드인지 확인
-			if (temp2->start + temp2->size == next2->start)
-			{
-				// temp2는 대상노드의 이전노드
-				// next2는 대상노드
-				temp2->link = next2->link; // 합병 대상 노드의 앞과 뒤를 연결
-				temp2->size += next2->size; // 이전노드의 size 증가
-				next2 = next2->link; // next2는 다음 노드로 이동
-			}
-			else
-			{
-				temp2 = next2; // 한칸씩 이동
-				next2 = next2->link;
-			}
-		}
-		break;
-	case 1: // 노드가 한개 -> 할 필요 없음
-		break;
-	}
+// ********************
+// @ byteToBitsString
+// @ 1 byte를 입력으로 받아, 길이 len 만큼 binary string을 만들어 반환
+// ********************
+string byteToBitsString(unsigned char oneByte, int len) {
+    string bitsString = ""; 
+    // binary string 만들고 반환
+    for (int i = 7; i >= 0; i--) {
+        if (len <= i) 
+            continue;
+        unsigned char po = pow(2, i);
+        if (oneByte >= po) {
+            bitsString += '1';
+            oneByte -= po;
+        }
+        else {
+            bitsString += '0';
+        }
+    }
+    return bitsString;
 }
 
-int display()
-{
-	Chunk* temp;
-	temp = head;
-	float success = total - fail;
-	if (temp == NULL)
-	{
-		printf("할당 성공률(공간 부족 없음): %f %% \n", success / total * 100);
-		printf("메모리가 가득 찼습니다!\n\n");
-		system("pause");
-	}
-	else
-	{
-		while (temp->link != NULL)
-		{
-			printf("start: %d size: %d \n", temp->start, temp->size);
-			temp = temp->link;
-		}
-		printf("start: %d size: %d \n", temp->start, temp->size);
-		printf("할당 성공률(공간 부족 없음): %f %% \n\n", success / total * 100);
-	}
-	return 0;
+
+// ********************
+// @ encoder
+// @ mapping된 characterMap, HuffmanInput.txt에서 읽은 string txt를 가져옴
+// @ txt에서 한글자씩 읽어 characterMap과 비교.
+// @ 각 글자에 해당하는 binary를 encoding_text.txt에 write
+// ********************
+void encoder(const string& txt, unordered_map<char, string>& characterMap) {
+    ofstream fout; //파일에 쓰기
+    string encodedTxt = ""; //인코딩될 내용을 저장
+
+    printf("===================== encoder시작 =======================\n");
+    fout.open("./encoding_text.txt", ios::out | ios::binary); // 압축할 파일 선택(write위한 type binary 파일 open)
+
+    // mapping된 characterMap 기록
+    size_t size = characterMap.size(); // 존재하는 문자 수 구하기
+    unsigned char ucSize = size; 
+    fout << ucSize; // 문자가 몇개 있는지 write
+    printf("문자 개수 = %d\n", ucSize);
+
+    // key, value bits len, value bits 기록
+    unsigned char uc = 0;
+    //encoding 시작
+    for (auto& i : characterMap) {
+        uc = i.first; //characterMap의 첫번째부터
+        fout << uc;  // 해당 문자를 write
+        //printf("문자 %c / ", uc);
+        unsigned char len = i.second.size();
+        fout << len;  // value bits 길이 기록
+        
+        //printf("%d", i.second.size());
+
+        // value bits 구하기
+        unsigned char summ = 0; 
+        for (size_t j = 0; j < len;) { 
+            //BITSNUMBYTE = 8
+            for (int k = 0; k < BITSNUMBYTE; k++, j++) {
+                if (j > len - 1) break; //문자의 길이를 초과하기전 반복 종료
+                summ = (summ << 1) | (i.second[j] & 1); // value bits 계산 과정
+            }
+            fout << summ; // value bits 기록
+            printf("summ = %d\n", summ);
+            summ = 0;
+        }
+    }
+
+    // 허프만 기법으로 encoding된 binary encodedTxt 만들기
+    size = txt.size(); 
+    for (size_t i = 0; i < size; i++) {
+        char currentCharacter = txt[i]; //기존의 txt파일을 하나하나 얻어서
+        encodedTxt += characterMap[currentCharacter]; //해당 문자를 encodedTxt에 추가(비트)   
+    }
+
+    // encodedTxt의 8bits를 1byte로 기록
+    unsigned char remainBit = 0; // bit 개수가 8의 배수가 아닐 경우, 나머지 bit처리
+    size = encodedTxt.size(); 
+    unsigned char sum = 0;
+    for (size_t i = 0; i < size - 1;) {
+        for (int k = 0; k < BITSNUMBYTE; k++, i++) {
+            if (i > size - 1) {
+                remainBit = (unsigned char)k;
+                break;
+            }
+            sum = (sum << 1) | (encodedTxt[i] & 1); 
+        }
+        fout << sum;
+        cout << "sum : " << bitset<4>(sum) << "\n";
+        sum = 0;
+    }
+
+    // remainBit는 file의 마지막에 기록
+    fout << remainBit;
+    cout << "reaminBit : " << bitset<4>(remainBit) << "\n";
+    fout.close();
 }
 
-void main()
+
+// ********************
+// @ decoder
+// @ encoding_text.txt를 불러옴
+// @ encoding_text.txt에서 한 bit씩 읽어 앞 부분의 map 읽고, map 복구. 
+// @ map 이후부터 encoding data 읽음.
+// @ 마지막이 8bits로 안 나눠 떨어질 수 있음. 남은 bits만큼 읽기.
+// ********************
+void decoder() {
+    ifstream fin;
+    ofstream fout;
+    fin.open("./encoding_text.txt", ios::in | ios::binary); //binary파일 읽기
+    fout.open("./HuffmanOutput.txt"); //파일 쓰기
+
+    // file의 길이 구하기
+    fin.seekg(0, ios::end); //파일의 첫위치에서 마지막으로 이동
+    int fileLen = fin.tellg(); //마지막의 위치를 fileLen에 저장
+    fin.seekg(0, ios::beg); //초기화
+
+    // character 개수 세기
+    int mapCount = 0; 
+    mapCount = fin.get(); //복구할 파일의 총 문자수 구하기
+    int hashLen = 1;
+
+    // characterMap 복구
+    unordered_map<string, char> characterMap;
+    for (int i = 0; i < mapCount; i++) {
+        char character = 0;
+        fin.get(character); //각각 character의 개수 구하기
+
+        hashLen += 2; 
+        int len = 0;
+        len = fin.get();
+
+        unsigned char value = 0;
+        string valueString = "";
+        for (int j = 0; j < len;) {
+            value = fin.get();
+            if (len > BITSNUMBYTE) {
+                valueString += byteToBitsString(value, BITSNUMBYTE);
+            }
+            else {
+                valueString += byteToBitsString(value, len);
+            }
+            hashLen++;
+            len -= BITSNUMBYTE;
+        }
+
+        characterMap[valueString] = character;
+        //printf("%c ", character);
+    }
+    printf("\n");
+
+    // encoding.txt 를 읽어 binary string인 txt를 만듦
+    fileLen -= hashLen;
+    unsigned char ucCharacter = 0;
+    string txt = "";
+    while (true) {
+        ucCharacter = fin.get();
+        if (fin.fail()) break;
+        if (fileLen > 2) {
+            txt += byteToBitsString(ucCharacter, BITSNUMBYTE);
+        }
+        else {
+            unsigned char remainBit = fin.get();
+            txt += byteToBitsString(ucCharacter, (int)remainBit);
+        }
+        fileLen -= 1;
+    }
+
+    // characterMap과 binary string인 txt를 비교하여 HuffmanOutput을 기록
+    string tmpTxt = "";
+    for (auto& i : txt) {
+        tmpTxt += i;
+        if (characterMap.count(tmpTxt)) {
+            fout << characterMap[tmpTxt];
+            tmpTxt = "";
+        }
+    }
+
+    fin.close();
+    fout.close();
+}
+
+
+int main()
 {
-	srand((unsigned int)time(NULL));
-	double start, end;
-	start = (double)clock() / CLOCKS_PER_SEC;
-	int i;
-	int index = 0;
-	Chunk available; // 할당및 저장을 할 연결리스트 available
-	Init(&available);
+    
+    ifstream fin;// input file sream == 파일을 프로그램에 넣을 수 있게 해준다
+    fin.open("./HuffmanInput.txt");// test.txt를 열기
 
-	vector <int> start_v;
-	vector <int> size_v;
-	vector <char> index_v;
+    // unorderd_map == map보다 더 빠른 해시테이블로 구현한 자료구조, 중복된 데이터 허용x, 데이터양 많을수록 유리, 유사데이터 처리는 힘듦
+    unordered_map<char, int> freqMap; // 빈도수를 저장해줄 hashmap
+    unordered_map<char, string> characterMap; // 문자를 저장해줄 charactermap
+    string txt = "";
 
-	for (int i = 0; i < 10; i++)
-	{
-		int size_temp = 0;
-		int start_temp = 0;
-		int choice;
-		choice = rand() % 4;
-		if (start_v.size() < 1)
-			choice = 0;
-		switch (choice)
-		{
-		case 0:
-		case 1:
-		case 2:
-			size_temp = (rand() % 99) + 1;
-			printf("할당할 용량 : %d\n", size_temp);
+    char character; 
+    while (true) {
+        fin.get(character); // get을 이용해 character를 얻고
+        if (fin.fail()) break; // 모든 문자를 다 읽으면 종료
+        freqMap[character]++; // hashmap을 이용해 해당 character의 빈도수를 추가
+        txt += character; // txt에 읽은 character를 넣어 최종적으로 file 내용 옮기기
+    }
 
-			size_v.push_back(size_temp);
-			start_v.push_back(myalloc(&available, size_temp));
-			//start_v.push_back(myalloc_best(&available, size_temp));
-			index_v.push_back(97 + index);
-			printf("%c: %d \n", 97 + index++, start_v[start_v.size() - 1]);
-			printf("%d\n", i);
+    priority_queue<Node, vector<Node>, cmp> p; // 우선순위 큐
+    // hashmap에 있는 character들을 2개씩 노드화 해서 넣어준다 -> 오름차순 정렬
+    // 이론적으론 내림차순이지만 큐의 특성상 pop의 사용을 위해 오름차순으로 정렬해준다.
+    for (auto& i : freqMap) {
+        Node node(i.first, i.second); 
+        p.push(node);
+    }
+    printf("\n");
 
-			if (start_v[start_v.size() - 1] == -1)
-			{
-				size_v.erase(size_v.begin() + size_v.size() - 1);
-				start_v.erase(start_v.begin() + start_v.size() - 1);
-				index_v.erase(index_v.begin() + index_v.size() - 1);
-				index--;
-			}
-			display();
+    makeTree(p); // 허프만트리 생성
+    characterMapping("", p.top(), characterMap); //character를 맵핑
 
-			break;
-		case 3:
-			start_temp = (rand() % start_v.size());
-			printf("free %c (기존 start = %d, 기존 size = %d)\n", index_v.at(start_temp), start_v.at(start_temp), size_v.at(start_temp));
-			myfree(&available, start_v.at(start_temp), size_v.at(start_temp));
-			display();
-			start_v.erase(start_v.begin() + start_temp);
-			size_v.erase(size_v.begin() + start_temp);
-			index_v.erase(index_v.begin() + start_temp);
+    /*
+    for (auto& i : characterMap) {
+        printf("%c", i.first);
+        cout << i.second << endl;
+    }
+    */
 
-			break;
-		default:
-			break;
-		}
-	}
+    encoder(txt, characterMap);
+    decoder();
 
-	end = (((double)clock()) / CLOCKS_PER_SEC);
-	printf("프로그램 수행 시간 :%lf 초\n", (end - start));
-	system("PAUSE");
+    fin.close();
 }
